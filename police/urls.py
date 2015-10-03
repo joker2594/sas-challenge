f5f895211528c2b65731ab68b220ea21ed8a7a53
2092020g <2092020g@student.gla.ac.uk>
2015-10-03 19:05:41 +0100

    edited script.js

--- police/urls.py
+++ police/urls.py
@@ -3,4 +3,6 @@ from police import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^index$', views.index, name='index'),
+        url(r'^deploy$', views.deploy, name='deploy'),
 ]
